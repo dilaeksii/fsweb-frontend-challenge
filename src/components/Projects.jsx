7389841654472d32dba7eab6a6efb71aabb6914b
dilaeksii <dilaeksii@gmail.com>
2026-01-14 12:28:43 +0300

    feat: make projects and skills dynamic using map

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,13 @@
-import profile from "../data/profile.json"
-import data from "../data/data.json"
+import profile from "../data/profile.json";
+import data from "../data/data.json";
+import projects from "../data/projects.json";
 import { useSetting } from "../SettingsContext";
 
 export const Projects = () => {
-  const {language} = useSetting();
+  const { language } = useSetting();
 
   const lang = data[language];
+  const projectList = projects[language].project;
 
   return (
     <>
@@ -13,55 +15,43 @@ export const Projects = () => {
         <h1 className="font-semibold text-[#1F2937] leading-[1.5] text-5xl">
           {lang.projects}
         </h1>
+
         <div className="grid grid-cols-3 pt-3 gap-y-15 gap-x-10 max-sm:flex max-sm:flex-col">
-          <div className="w-[300px]">
-            <img src="src/images/project-1.png" alt="Project1" />
-            <p className="text-[#4338CA] text-3xl font-medium py-3">Workintech</p>
-            <p className="font-normal text-sm text-[#6B7280]">
-              {lang.project1}
-            </p>
-            <div className="flex gap-1 py-3">
-                <p className="border rounded-sm border-[#3730A3] py-[6px] px-[18px] text-[#3730A3]">react</p>
-                <p className="border rounded-sm border-[#3730A3] py-[6px] px-[18px] text-[#3730A3]">redux</p>
-                <p className="border rounded-sm border-[#3730A3] py-[6px] px-[18px] text-[#3730A3]">axios</p>
-            </div>
-            <div className="flex justify-between py-3">
-                <a href={profile.github} target="_blank" className="text-[#3730A3] underline">Github</a>
-                <a href="#" className="text-[#3730A3] underline">{lang.view}</a>
-            </div>
-          </div>
-          <div className="w-[300px]">
-            <img src="src/images/project-2.png" alt="Project2" />
-            <p className="text-[#4338CA] text-3xl font-medium py-3">Random Jokes</p>
-            <p className="font-normal text-sm text-[#6B7280]">
-              {lang.project1}
-            </p>
-            <div className="flex gap-1 py-3">
-                <p className="border rounded-sm border-[#3730A3] py-[6px] px-[18px] text-[#3730A3]">react</p>
-                <p className="border rounded-sm border-[#3730A3] py-[6px] px-[18px] text-[#3730A3]">redux</p>
-                <p className="border rounded-sm border-[#3730A3] py-[6px] px-[18px] text-[#3730A3]">axios</p>
-            </div>
-            <div className="flex justify-between py-3">
-                <a href={profile.github} target="_blank" className="text-[#3730A3] underline">Github</a>
-                <a href="#" className="text-[#3730A3] underline">{lang.view}</a>
-            </div>
-          </div>
-          <div className="w-[300px]">
-            <img src="src/images/project-3.png" alt="Project3" />
-            <p className="text-[#4338CA] text-3xl font-medium py-3">Journey</p>
-            <p className="font-normal text-sm text-[#6B7280]">
-              {lang.project1}
-            </p>
-            <div className="flex gap-1 py-3">
-                <p className="border rounded-sm border-[#3730A3] py-[6px] px-[18px] text-[#3730A3]">react</p>
-                <p className="border rounded-sm border-[#3730A3] py-[6px] px-[18px] text-[#3730A3]">redux</p>
-                <p className="border rounded-sm border-[#3730A3] py-[6px] px-[18px] text-[#3730A3]">axios</p>
-            </div>
-            <div className="flex justify-between py-3">
-                <a href={profile.github} target="_blank" className="text-[#3730A3] underline">Github</a>
-                <a href="#" className="text-[#3730A3] underline">{lang.view}</a>
-            </div>
-          </div>
+          {projectList.map((prj) => {
+            return (
+              <div className="w-[300px]" key={prj.id}>
+                <img src={prj.img} alt="Project1" />
+                <p className="text-[#4338CA] text-3xl font-medium py-3">
+                  {prj.title}
+                </p>
+                <p className="font-normal text-sm text-[#6B7280]">{prj.desc}</p>
+                <div className="flex gap-1 py-3">
+                  {prj.tech.map((item, index) => {
+                    return (
+                      <p
+                        className="border rounded-sm border-[#3730A3] py-[6px] px-[18px] text-[#3730A3]"
+                        key={index}
+                      >
+                        {item}
+                      </p>
+                    );
+                  })}
+                </div>
+                <div className="flex justify-between py-3">
+                  <a
+                    href={profile.github}
+                    target="_blank"
+                    className="text-[#3730A3] underline"
+                  >
+                    Github
+                  </a>
+                  <a href="#" className="text-[#3730A3] underline">
+                    {lang.view}
+                  </a>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,35 +1,32 @@
 import data from "../data/data.json";
 import { useSetting } from "../SettingsContext";
+import skills from "../data/skills.json";
 
 export const Skills = () => {
   const { language } = useSetting();
 
   const lang = data[language];
+  const skillsList = skills[language].skill;
+
   return (
     <>
       <div className="pt-15 pb-10">
         <h1 className="font-semibold text-[#1F2937] leading-[1.5] text-5xl">
-          Skills
+          {lang.skills}
         </h1>
         <div className="grid grid-cols-3 max-sm:flex max-sm:flex-col">
-          <div className="w-[300px] pt-5">
-            <h1 className="text-[#4338CA] text-3xl font-medium">Java Script</h1>
-            <p className="text-left text-xs font-normal text-[#6B7280] pt-5">
-              {lang.javasc}
-            </p>
-          </div>
-          <div className="w-[300px] pt-5">
-            <h1 className="text-[#4338CA] text-3xl font-medium">React.JS</h1>
-            <p className="text-left text-xs font-normal text-[#6B7280] pt-5">
-              {lang.react}
-            </p>
-          </div>
-          <div className="w-[300px] pt-5">
-            <h1 className="text-[#4338CA] text-3xl font-medium">Node.JS</h1>
-            <p className="text-left text-xs font-normal text-[#6B7280] pt-5">
-              {lang.node}
-            </p>
-          </div>
+          {skillsList.map((skill) => {
+            return (
+              <div className="w-[300px] pt-5" key={skill.id}>
+                <h1 className="text-[#4338CA] text-3xl font-medium">
+                  {skill.title}
+                </h1>
+                <p className="text-left text-xs font-normal text-[#6B7280] pt-5">
+                  {skill.desc}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
